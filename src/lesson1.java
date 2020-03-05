7ac042884e771cc5e9d9b7c770a19bd45082355b
apollinaria-z <polina.zolotaya@gmail.com>
2020-03-05 21:31:16 +0300

    first commit

---
+++ src/lesson1.java
@@ -0,0 +1,20 @@
+public class lesson1
+{
+    public static void main(String[] args) {
+        int[] goals = new int [5];
+        goals[0] =2;
+        goals[1] =1;
+        goals[2] =0;
+        goals[3] =5;
+        goals[4] =3;
+        for (int i=0;i< goals.length;i++){
+
+        System.out.println("забил в "+(i+1) +"-м матче->"+ goals[i]);}
+        System.out.println("Демонстрация Switch");
+           for (int i = 0;i < goals. length;i++){
+               System.out.println("В " +(i+1)+"-м матче");
+               switch (goals [i])
+           }
+    }
+}
+
